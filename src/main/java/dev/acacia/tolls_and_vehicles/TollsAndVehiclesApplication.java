28d66730832c4629bfea2ebf206ed6c2255a031b
Acacia-Sanchez-Pastur <sanchezpastur@gmail.com>
2024-11-26 23:45:10 +0100

    first commit

---
+++ src/main/java/dev/acacia/tolls_and_vehicles/TollsAndVehiclesApplication.java
@@ -0,0 +1,13 @@
+package dev.acacia.tolls_and_vehicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollsAndVehiclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TollsAndVehiclesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/acacia/tolls_and_vehicles/TollsAndVehiclesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.acacia.tolls_and_vehicles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TollsAndVehiclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
